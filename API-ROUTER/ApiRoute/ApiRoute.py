89c5ceadafe4e1b0c63a3d5e6f82dad68a4f61db
CBC <39294675+cbccbs@users.noreply.github.com>
2022-05-03 15:36:25 +0900

    feat: API Service 호출 기능 추가
    AIPLATFORM-227

--- API-ROUTER/ApiRoute/ApiRoute.py
+++ API-ROUTER/ApiRoute/ApiRoute.py
@@ -8,6 +8,7 @@ from Utils.DataBaseUtil import convert_data
 from Utils.CommonUtil import connect_db, make_res_msg, save_file_for_reload
 from pydantic import BaseModel
 from starlette.requests import Request
+import requests
 import traceback
 
 #logger = logging.getLogger()
@@ -24,7 +25,6 @@ class ApiInfo(BaseModel):
     url: str
     msg_type: str
     method: str
-    protocol: str
     command: str
     bypass: str
     params: List[ApiParam]
@@ -44,7 +44,7 @@ class ApiRoute:
         api_info, _ = db.select('SELECT * FROM api_info;')
             
         for api in api_info:
-            self.router.add_api_route(f'/api/{api["api_name"]}', self.route_api, methods=["POST"], tags=["route"])
+            self.router.add_api_route(f'/route/{api["api_name"]}', self.route_api, methods=[api["method"]], tags=["route"])
         
         for api_name, api_info in config.api_config.items():
             module_path = f'{config.root_path}/API-ROUTER/ApiList/{api_name}.py'
@@ -52,7 +52,7 @@ class ApiRoute:
             spec = importlib.util.spec_from_file_location(module_name, module_path)
             module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(module)
-            self.router.add_api_route(f'{api_info["url_prefix"]}/{api_name}', module.api, methods=[api_info["method"]], tags=["service"])
+            self.router.add_api_route(f'{api_info["url_prefix"]}/{api_info["sub_dir"]}/{api_name}', module.api, methods=[api_info["method"]], tags=["service"])
         
 
     def get_api_list(self) -> Dict:
@@ -98,7 +98,9 @@ class ApiRoute:
                 insert_api_info[key] = value
 
         db.insert("api_info", [insert_api_info])
-        db.insert("api_params", insert_api_params)
+
+        if len(insert_api_params) != 0:
+            db.insert("api_params", insert_api_params)
         
         save_file_for_reload()
             
@@ -115,10 +117,15 @@ class ApiRoute:
         return {"API_NAME : del_api"}
 
     async def route_api(self, request:Request) -> Dict:
+        result = None
         try:
             api_name = request.url.path.split("/")[-1]
+            method = request.method
             api_info_query = f'SELECT * FROM api_info WHERE api_name = {convert_data(api_name)};'        
             api_params_query = f'SELECT * FROM api_params WHERE api_name = {convert_data(api_name)};'
+
+            print(f'api_name : {api_name}')
+            print(f'method : {method}')
 
             db = connect_db(config.db_type, config.db_info)        
             api_info, _ = db.select(api_info_query)
@@ -129,24 +136,43 @@ class ApiRoute:
  
             api_info = api_info[0]
             msg_type = api_info["msg_type"]
-            if msg_type == "JSON":
-                request_body = await request.json()
-            elif msg_type == "BINARY":
-                request_body = await request.form() #request.body()
-            else:
-                # Unknown Type
-                pass    
-    
+                       
             if api_info["bypass"] == "ON":
-                #send req
+                #send API-SERVICE
                 method = api_info["method"]
-            else:
-                #call remote func
+                if method == "GET":
+                    params_query = request.query_params
+                    params_query = str(params_query)
+                    params = {}
+                    if len(params_query) != 0:
+                        for param in params_query.split("&"):
+                            parser_param = param.split("=")
+                            params[parser_param[0]] = parser_param[1] 
+                    response = requests.get(api_info["url"], params=params)
+                elif method == "POST":
+                    if msg_type == "JSON":
+                        body = await request.json()
+                        response = requests.post(api_info["url"], json=body)
+                    else:
+                        body = await request.form()
+                        response = requests.post(api_info["url"], data=body)                        
+                elif method == "PUT":
+                    if msg_type == "JSON":
+                        body = await request.json()
+                        response = requests.put(api_info["url"], json=body)
+                    else:
+                        body = await request.form()
+                        response = requests.put(api_info["url"], data=body)                   
+                else:
+                    print("Not Implemented Method.")
+                result = response.json()
+            else: # bypass "OFF"
+                #call REMOTE FUNCTION
                 remote_cmd = RemoteCmd(config.remote_info["host"], config.remote_info["port"], config.remote_info["id"], config.remote_info["password"])
                 #make command (use params + body)
-                remote_cmd.cmd_exec(api_info["command"])
+                result = remote_cmd.cmd_exec(api_info["command"])
         except Exception:
             print(traceback.format_exc())
-        return eval(remote_cmd.cmd_exec(api_info["command"]))
+        return result
     
        
