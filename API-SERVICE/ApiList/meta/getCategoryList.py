7c8162b582593ec5c12761730e3de307aaf468fd
lum0380 <lum0380@mobigen.com>
2022-05-04 11:55:31 +0900

    [AIPLATFORM-225] feat: getCategoryList 모듈 추가

--- API-SERVICE/ApiList/meta/getCategoryList.py
+++ API-SERVICE/ApiList/meta/getCategoryList.py
@@ -3,16 +3,11 @@ from ApiService.ApiServiceConfig import config
 from Utils.CommonUtil import connect_db
 from Utils.DataBaseUtil import convert_data
 
-'''
-    <!-- category 목록 조회-->
-    <select id="getCategoryList" resultType="java.util.Map">
-        select *
-        from metasch.tb_category
-        order by parent_id, node_id;
-    </select>
-'''
 
 def api() -> Dict:
     db = connect_db(config.db_type, config.db_info)
-
-    return {"API_NAME" : "TEST"}
+    category_query = "select * \
+                      from metasch.tb_category \
+                      order by parent_id, node_id;"
+    category_list = db.select(category_query)[0]
+    return {"result" : "", "errorMessage" : "", "data": category_list}
