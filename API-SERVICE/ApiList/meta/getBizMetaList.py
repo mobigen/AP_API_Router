86ddf0063aa3c6c2211828159bf8f79ad92bd53a
lum0380 <lum0380@mobigen.com>
2022-05-04 14:26:01 +0900

    [AIPLATFORM-225] fix: 오타 및 출력 순서 수정

--- API-SERVICE/ApiList/meta/getBizMetaList.py
+++ API-SERVICE/ApiList/meta/getBizMetaList.py
@@ -3,21 +3,6 @@ from ApiService.ApiServiceConfig import config
 from Utils.CommonUtil import connect_db
 from Utils.DataBaseUtil import convert_data
 
-'''
-    <!-- meta form list 목록 조회-->
-    <select id="getBizMetaList" resultType="java.util.Map">
-        select T.biz_dataset_id      as rowId,
-               array_agg(T.item_val) as data,
-               array_agg(T.item_id)  as columnKey
-        from (select biz_dataset_id, tbm.item_id, tbm.item_val, tbmm.name_id, kor_name, eng_name
-              from metasch.tb_biz_meta tbm
-                       right join metasch.tb_biz_meta_map tbmm on tbm.item_id = tbmm.item_id
-                       left join metasch.tb_biz_meta_name tbmn on tbmm.name_id = tbmn.name_id
-              order by biz_dataset_id, item_id) T
-        group by biz_dataset_id
-        order by biz_dataset_id;
-    </select>
-'''
 
 def api() -> Dict:
     db = connect_db(config.db_type, config.db_info)
@@ -34,9 +19,8 @@ def api() -> Dict:
         order by biz_dataset_id;
     """
     bizmeta_list = db.select(meta_name_query)
-    print(bizmeta_list)
     
     v_meta_map_query = "SELECT kor_name,eng_name,name_id FROM tb_biz_meta_name;"
     v_meta_map = db.select(v_meta_map_query)
 
-    return {"result" : "", "errorMessage" : "", "data": {"body": bizmeta_list[0],"header":v_meta_map_query[0]}}
+    return {"result" : "", "errorMessage" : "", "data": {"body": bizmeta_list[0],"header":v_meta_map[0]}}
--- API-SERVICE/ApiList/meta/useMetaNameList.py
+++ API-SERVICE/ApiList/meta/useMetaNameList.py
@@ -6,7 +6,23 @@ from Utils.DataBaseUtil import convert_data
 
 def api() -> Dict:
     db = connect_db(config.db_type, config.db_info)
-    meta_name_query = "SELECT * FROM tb_biz_meta_name;"
+    meta_name_query = """        
+        select                
+            case
+                when (select tbmm.name_id from tb_biz_meta_map tbmm where tbmn.name_id = tbmm.name_id) is null then 0
+                else 1
+                end as use_meta,
+            tbmn.kor_name,
+            tbmn.eng_name,
+            tbmn.show_order,
+            case
+                when tbmn.type = 0 then 'text'
+                when tbmn.type = 1 then 'int'
+                when tbmn.type = 2 then 'binary'
+                end as type,
+            tbmn.name_id
+        from tb_biz_meta_name tbmn
+        order by tbmn.name_id;"""
     meta_name = db.select(meta_name_query)
 
     return {"result" : "", "errorMessage" : "", "data": {"body": meta_name[0]}}
