745aef22e2f63445df2c95139ce84ec39f00dd22
lum0380 <lum0380@mobigen.com>
2022-05-04 11:32:15 +0900

    [AIPLATFORM-225] feat: insertBizMeta 모듈 추가

--- API-SERVICE/ApiList/meta/insertBizMeta.py
+++ API-SERVICE/ApiList/meta/insertBizMeta.py
@@ -1,11 +1,23 @@
+import uuid
 from typing import Dict
 from ApiService.ApiServiceConfig import config
 from Utils.CommonUtil import connect_db
 from Utils.DataBaseUtil import convert_data
 
-'''
-'''
+
 def api() -> Dict:
     db = connect_db(config.db_type, config.db_info)
+    query = "SELECT biz_dataset_id FROM tb_biz_meta;"
+    biz_meta_list = db.select(query)[0]
 
-    return {"API_NAME" : "TEST"}
+    for biz_meta in biz_meta_list:        
+        query = f'UPDATE tb_biz_meta\
+                  SET biz_dataset_id = ({convert_data(uuid.uuid4())})\
+                  WHERE biz_dataset_id = {convert_data(biz_meta["biz_dataset_id"])};'
+        db.execute(query)
+    
+    query = "SELECT item_id as itemId, item_val as itemVal FROM tb_biz_meta;"
+    biz_meta_list = db.select(query)[0]
+    
+    return biz_meta_list
+
