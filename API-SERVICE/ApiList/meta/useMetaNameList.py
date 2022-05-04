1bb706b7382e01fef84398f23869122bdc9a0dfe
lum0380 <lum0380@mobigen.com>
2022-05-04 10:31:31 +0900

    [AIPLATFORM-225] feat: useMetaNameList 모듈 추가

--- API-SERVICE/ApiList/meta/useMetaNameList.py
+++ API-SERVICE/ApiList/meta/useMetaNameList.py
@@ -3,9 +3,10 @@ from ApiService.ApiServiceConfig import config
 from Utils.CommonUtil import connect_db
 from Utils.DataBaseUtil import convert_data
 
-'''
-'''
+
 def api() -> Dict:
     db = connect_db(config.db_type, config.db_info)
+    meta_name_query = "SELECT * FROM tb_biz_meta_name;"
+    meta_name = db.select(meta_name_query)
 
-    return {"API_NAME" : "TEST"}
+    return {"result" : "", "errorMessage" : "", "data": {"body": meta_name[0]}}
