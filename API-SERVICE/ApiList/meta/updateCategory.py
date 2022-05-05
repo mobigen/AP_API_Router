07685007c185b28160b751a5e48653fd1fc66ba7
lum0380 <lum0380@mobigen.com>
2022-05-06 08:27:10 +0900

    [AIPLATFORM-225] fix: updateCategory 모듈 수정

--- API-SERVICE/ApiList/meta/updateCategory.py
+++ API-SERVICE/ApiList/meta/updateCategory.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 from ApiService.ApiServiceConfig import config
 from Utils.CommonUtil import connect_db
 from Utils.DataBaseUtil import convert_data
@@ -6,19 +6,28 @@ from pydantic import BaseModel
 
 
 class UpdateCategory(BaseModel):
-    parent_id: str
-    node_id: str
+    parent_id: Optional[str] = None
+    node_id: Optional[str] = None
     node_name: str
 
 
 # todo: 수정 필요
 def api(update:UpdateCategory) -> Dict:
     db = connect_db(config.db_type, config.db_info)
-    query = f'UPDATE tb_biz_meta_name\
-                SET parent_id   = {convert_data(update.parent_id)},\
-                    node_id   = {convert_data(update.node_id)},\
-                    node_name = {convert_data(update.node_name)}\
-                WHERE name_id = {convert_data(update.node_id)};'\
+    
+    if update.node_id:    
+        query = f'UPDATE tb_biz_meta_name\
+                  SET parent_id   = {convert_data(update.parent_id)},\
+                      node_id   = {convert_data(update.node_id)},\
+                      node_name = {convert_data(update.node_name)}\
+                  WHERE node_id = {convert_data(update.node_id)};'
+    else:
+        query = f'UPDATE tb_biz_meta_name\
+                  SET parent_id   = {convert_data(update.parent_id)},\
+                      node_id   = {convert_data(update.node_id)},\
+                      node_name = {convert_data(update.node_name)}\
+                  WHERE parent_id = {convert_data(update.parent_id)} and \
+                        node_name = {convert_data(update.node_name)};'
 
     db.execute(query)
     return {"result" : "", "errorMessage" : ""}
