6100d11a4534d0ffc15d4d905856301857760c78
lum0380 <lum0380@mobigen.com>
2022-05-06 10:12:37 +0900

    [AIPLATFORM-225] fix: insertBizMeta update qury에서 insert로 변경

--- API-SERVICE/ApiList/meta/insertBizMeta.py
+++ API-SERVICE/ApiList/meta/insertBizMeta.py
@@ -5,15 +5,13 @@ from Utils.CommonUtil import connect_db
 from Utils.DataBaseUtil import convert_data
 
 
-def api() -> Dict:
+def api(biz_meta_list:list) -> Dict:
     db = connect_db(config.db_type, config.db_info)
-    query = "SELECT biz_dataset_id FROM tb_biz_meta;"
-    biz_meta_list = db.select(query)[0]
-
+    uid = uuid.uuid4()
     for biz_meta in biz_meta_list:        
-        query = f'UPDATE tb_biz_meta\
-                  SET biz_dataset_id = ({convert_data(uuid.uuid4())})\
-                  WHERE biz_dataset_id = {convert_data(biz_meta["biz_dataset_id"])};'
+        query = f'INSERT INTO tb_biz_meta (biz_dataset_id,item_id,item_val)\
+                VALUES ({convert_data(uid)},{convert_data(biz_meta["itemId"])},{convert_data(biz_meta["itemVal"])});'
+    
         db.execute(query)
     
     query = "SELECT item_id as itemId, item_val as itemVal FROM tb_biz_meta;"
