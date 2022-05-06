db587b600d381c89706fc87afb3e5e08e4c2fe48
lum0380 <lum0380@mobigen.com>
2022-05-06 10:12:04 +0900

    [AIPLATFORM-225] feat: addChildCategory 모듈 추가

---
+++ API-SERVICE/ApiList/meta/addChildCategory.py
@@ -0,0 +1,21 @@
+import uuid
+from typing import Dict, Optional
+from ApiService.ApiServiceConfig import config
+from Utils.CommonUtil import connect_db
+from Utils.DataBaseUtil import convert_data
+from pydantic import BaseModel
+
+
+class addChildCategory(BaseModel):
+    parent_id: str
+    node_name: str
+
+
+# todo: 수정 필요
+def api(update:addChildCategory) -> Dict:
+    db = connect_db(config.db_type, config.db_info)
+    query = f'INSERT INTO tb_category (node_name, parent_id, node_id)\
+              VALUES ({convert_data(update.node_name)},{convert_data(update.parent_id)},{convert_data(uuid.uuid4())});'
+
+    db.execute(query)
+    return {"result" : "", "errorMessage" : ""}
