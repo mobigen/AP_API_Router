64b29494ed11610640a90d678163231a15213173
lum0380 <lum0380@mobigen.com>
2022-05-06 10:13:38 +0900

    [AIPLATFORM-225] fix: addChildCategory 기능과 분리

--- API-SERVICE/ApiList/meta/updateCategory.py
+++ API-SERVICE/ApiList/meta/updateCategory.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Dict, Optional
 from ApiService.ApiServiceConfig import config
 from Utils.CommonUtil import connect_db
@@ -6,28 +7,18 @@ from pydantic import BaseModel
 
 
 class UpdateCategory(BaseModel):
-    parent_id: Optional[str] = None
-    node_id: Optional[str] = None
+    node_id: str
     node_name: str
 
 
 # todo: 수정 필요
 def api(update:UpdateCategory) -> Dict:
     db = connect_db(config.db_type, config.db_info)
-    
-    if update.node_id:    
-        query = f'UPDATE tb_biz_meta_name\
-                  SET parent_id   = {convert_data(update.parent_id)},\
-                      node_id   = {convert_data(update.node_id)},\
-                      node_name = {convert_data(update.node_name)}\
-                  WHERE node_id = {convert_data(update.node_id)};'
-    else:
-        query = f'UPDATE tb_biz_meta_name\
-                  SET parent_id   = {convert_data(update.parent_id)},\
-                      node_id   = {convert_data(update.node_id)},\
-                      node_name = {convert_data(update.node_name)}\
-                  WHERE parent_id = {convert_data(update.parent_id)} and \
-                        node_name = {convert_data(update.node_name)};'
+    query = f'UPDATE tb_category\
+                SET parent_id   = {convert_data(uuid.uuid4())},\
+                    node_id   = {convert_data(update.node_id)},\
+                    node_name = {convert_data(update.node_name)}\
+                WHERE node_id = {convert_data(update.node_id)};'
 
     db.execute(query)
     return {"result" : "", "errorMessage" : ""}
