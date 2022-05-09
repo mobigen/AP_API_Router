09080285429b880debabedd08a58b3c86ffbe1bd
lum0380 <lum0380@mobigen.com>
2022-05-09 11:15:53 +0900

    [AIPLATFORM-225] feat: 페이징 기능 추가

--- API-SERVICE/ApiList/meta/metaNameList.py
+++ API-SERVICE/ApiList/meta/metaNameList.py
@@ -2,11 +2,32 @@ from select import select
 from typing import Dict
 from ApiService.ApiServiceConfig import config
 from Utils.CommonUtil import connect_db
+from Utils.DataBaseUtil import convert_data
 
-def api() -> Dict:
+def api(perPage:int, curPage:int) -> Dict:
     db = connect_db(config.db_type, config.db_info)
-
-    meta_name_query = "SELECT * FROM tb_biz_meta_name;"
+    
+    curPage = curPage - 1
+    meta_name_query = f"""
+    select *
+    from metasch.tb_biz_meta_name as p
+      join (SELECT kor_name,
+            eng_name,
+            show_order,
+            name_id,
+            (case
+                  when type = 0 then 'text'
+                  when type = 1 then 'int'
+                  when type = 2 then 'binary'
+                end
+                ) as type,
+            ROW_NUMBER () OVER (ORDER BY name_id DESC) as rowNo
+      FROM tb_biz_meta_name
+      order by name_id
+          limit {perPage}
+          offset ({perPage} * {curPage})
+      ) as t on p.name_id = t.name_id
+    """
     meta_name = db.select(meta_name_query)
 
     v_meta_name_query = "SELECT * FROM v_biz_meta_name;"
