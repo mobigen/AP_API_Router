69674b15d6f39906c3870e1a54c6c4b8b952dca6
lum0380 <cbccbs@mobigen.com>
2022-05-09 13:45:57 +0900

    [AIPLATFORM-225] chore: 변수명 수정, import 정리

--- API-SERVICE/ApiList/meta/metaNameList.py
+++ API-SERVICE/ApiList/meta/metaNameList.py
@@ -2,7 +2,6 @@ from select import select
 from typing import Dict
 from ApiService.ApiServiceConfig import config
 from Utils.CommonUtil import connect_db
-from Utils.DataBaseUtil import convert_data
 
 def api(perPage:int, curPage:int) -> Dict:
     db = connect_db(config.db_type, config.db_info)
