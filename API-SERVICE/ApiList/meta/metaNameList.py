7abbda65477e5e811dbe2e95ca95326da9c3b7b6
LeeYumi <lum0380@naver.com>
2022-05-12 17:27:53 +0900

    [AIPLATFORM-250] feat: totalCount 추가

--- API-SERVICE/ApiList/meta/metaNameList.py
+++ API-SERVICE/ApiList/meta/metaNameList.py
@@ -7,30 +7,34 @@ from fastapi.logger import logger
 def api(perPage: int, curPage: int) -> Dict:
     curPage = curPage - 1
     meta_name_query = f"""
-    select *
-    from meta.tb_biz_meta_name as p
-      join (SELECT kor_name,
-            eng_name,
-            show_order,
-            name_id,
-            (case
-                  when type = 0 then 'text'
-                  when type = 1 then 'int'
-                  when type = 2 then 'binary'
-                end
-                ) as type,
-            ROW_NUMBER () OVER (ORDER BY name_id DESC) as rowNo
-      FROM tb_biz_meta_name
-      order by name_id
-          limit {perPage}
-          offset ({perPage} * {curPage})
-      ) as t on p.name_id = t.name_id
+        select
+            *
+        from tb_biz_meta_name as p
+        join (
+            SELECT kor_name,
+                   eng_name,
+                   show_order,
+                   name_id,
+                   (case
+                       when type = 0 then 'text'
+                       when type = 1 then 'int'
+                       when type = 2 then 'binary'
+                       end
+                    ) as type,
+                    ROW_NUMBER () OVER (ORDER BY name_id DESC) as rowNo
+            FROM tb_biz_meta_name
+            order by name_id
+            limit {perPage}
+            offset ({perPage} * {curPage})
+        ) as t on p.name_id = t.name_id
     """
+    total_cnt_query = "SELECT count(*) as totalCount FROM tb_biz_meta_name"
     v_meta_name_query = "SELECT * FROM v_biz_meta_name;"
 
     try:
         db = connect_db(config.db_type, config.db_info)
         meta_name = db.select(meta_name_query)
+        total_cnt = db.select(total_cnt_query)
         v_meta_name = db.select(v_meta_name_query)
     except Exception as err:
         # make error response
@@ -38,6 +42,7 @@ def api(perPage: int, curPage: int) -> Dict:
         logger.error(err)
     else:
         # make response
-        result = {"result": "", "errorMessage": "", "data": {
-            "body": meta_name[0], "header": v_meta_name[0]}}
+        data = total_cnt[0][0]
+        data.update({"body": meta_name[0], "header": v_meta_name[0]})
+        result = {"result": "", "errorMessage": "", "data": data}
     return result
