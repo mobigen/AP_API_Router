cce7f9b66b5c96dfdd76f628af0ac5584b63a5ce
LeeYumi <lum0380@naver.com>
2022-06-20 15:52:19 +0900

    [AIPLATFORM-367] feat: item_id를 부여 할 수 있도록 수정, biz_dataset_id 소문자로 변경

--- API-SERVICE/ApiList/meta/insertMetaMap.py
+++ API-SERVICE/ApiList/meta/insertMetaMap.py
@@ -1,45 +1,55 @@
-from fastapi.logger import logger
+import uuid
 from typing import Dict
+from pydantic import BaseModel
+from fastapi.logger import logger
+from starlette.requests import Request
 from ApiService.ApiServiceConfig import config
 from Utils.CommonUtil import connect_db, get_token_info
 from Utils.DataBaseUtil import convert_data
-from starlette.requests import Request
 
 
-def api(map_list: list, request: Request) -> Dict:
+class NmIdList(BaseModel):
+    nm_id_list: list
+
+
+def api(insert: NmIdList, request: Request) -> Dict:
     user_info = get_token_info(request.headers)
 
-    view_col = ['"BIZ_DATASET_ID"']
-    drop_view_query = "DROP VIEW v_biz_meta_wrap"
-    truncate_query = "TRUNCATE tb_biz_meta_map;"
+    view_col = ['"BIZ_DATASET_ID" as biz_dataset_id']
+    drop_view_query = "DROP VIEW IF EXISTS v_biz_meta_wrap"
     map_insert_query = 'INSERT INTO tb_biz_meta_map ("ITEM_ID", "NM_ID") VALUES ({0}, {1});'
     meta_map_query = "SELECT * FROM tb_biz_meta_map"
+    nm_id_query = 'SELECT "NM_ID" FROM tb_biz_meta_map'
+
     map_item_query = """
-        select distinct 
-            cast(meta_map."ITEM_ID" as INT) as ITEM_ID,
+        select distinct
+            meta_map."ITEM_ID",
             tbmn."ENG_NM"
         from
             tb_biz_meta_name tbmn
         left join tb_biz_meta_map meta_map on
             tbmn."NM_ID" = meta_map."NM_ID"
-        order by ITEM_ID asc
+        where "ITEM_ID" IS NOT NULL
     """
 
     try:
         db = connect_db(config.db_info)
-        db.execute(drop_view_query)
-        db.execute(truncate_query)
 
-        for meta_map in map_list:
-            logger.info(meta_map)
-            db.execute(map_insert_query.format(convert_data(meta_map["ITEM_ID"]),
-                                               convert_data(meta_map["NM_ID"])))
+        nm_id_set = {_["NM_ID"] for _ in db.select(nm_id_query)[0]}
+        nm_id_set = set(insert.nm_id_list) - nm_id_set
+
+        for nm_id in nm_id_set:
+            db.execute(map_insert_query.format(convert_data(uuid.uuid4()),
+                                               convert_data(nm_id)))
+
+        # drop view v_biz_meta_wrap
+        db.execute(drop_view_query)
 
         # create view v_biz_meta_wrap
         meta_map_item = db.select(map_item_query)[0]
         for i, meta_map in enumerate(meta_map_item):
-            eng_name = meta_map["ENG_NM"]
-            col_format = f'\t\tmax(case when "ITEM_ID" = {convert_data(i + 1)} then "ITEM_VAL" end) as {eng_name}'
+            col_format = f'\t\tmax(case when "ITEM_ID" = {convert_data(meta_map["ITEM_ID"])} ' \
+                         f'then "ITEM_VAL" end) as {meta_map["ENG_NM"]}'
             view_col.append(col_format)
 
         view_col = ',\n'.join(view_col)
@@ -54,7 +64,7 @@ def api(map_list: list, request: Request) -> Dict:
 
         # return data
         meta_map_list = db.select(meta_map_query)[0]
-    # 수정 해야함
+
     except Exception as err:
         result = {"result": 0, "errorMessage": err}
         logger.error(err)
