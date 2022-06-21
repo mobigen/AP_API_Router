d9e535874e2218d208e49aca3d652861bde3627c
LeeYumi <lum0380@naver.com>
2022-06-21 11:18:02 +0900

    [AIPLATFORM-367] feat: eng_nm 중복된 항목 및 특수문자 예외처리

--- API-SERVICE/ApiList/meta/insertMetaName.py
+++ API-SERVICE/ApiList/meta/insertMetaName.py
@@ -1,4 +1,5 @@
 import uuid
+import string
 from typing import Dict
 from fastapi.logger import logger
 from ApiService.ApiServiceConfig import config
@@ -15,15 +16,38 @@ class InsertMetaName(BaseModel):
 
 
 def api(insert: InsertMetaName, request: Request) -> Dict:
+    # ENG_NM 중복 체크, 특수문자 체크
+    
     user_info = get_token_info(request.headers)
-
-    query = f'INSERT INTO tb_biz_meta_name ("KOR_NM", "ENG_NM", "SHOW_ODRG", "NM_ID", "TYPE")\
+    symbol_list = list(map(str,string.punctuation))
+    symbol_list.remove("_")
+    symbol_list.remove("'")
+    symbol_list.remove('"')
+    symbol_list.remove("-")
+    select_eng_nm = 'SELECT "ENG_NM" FROM tb_biz_meta_name'
+    insert_meta_name = f'INSERT INTO tb_biz_meta_name ("KOR_NM", "ENG_NM", "SHOW_ODRG", "NM_ID", "TYPE")\
               VALUES ({convert_data(insert.KOR_NM)}, {convert_data(insert.ENG_NM)}, 0,\
               {convert_data(uuid.uuid4())}, {convert_data(insert.TYPE)});'
     try:
         db = connect_db(config.db_info)
-        db.execute(query)
+        eng_nm_list = db.select(select_eng_nm)[0]
+        logger.debug(eng_nm_list)
+
+        # 중복 체크
+        if len(eng_nm_list):
+            eng_nm_list = [eng_nm["ENG_NM"] for eng_nm in eng_nm_list]
+            if insert.ENG_NM in eng_nm_list:
+                raise ValueError
+
+        # 특수문자 체크
+        if list(filter(lambda eng_nm: eng_nm in symbol_list,insert.ENG_NM)):
+            raise ValueError
+
+        db.execute(insert_meta_name)
     except Exception as err:
+        result = {"result": 0, "errorMessage": err}
+        logger.error(err)
+    except ValueError as err:
         result = {"result": 0, "errorMessage": err}
         logger.error(err)
     else:
