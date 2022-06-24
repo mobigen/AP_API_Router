76c26bcec396780b568d9cea1045015eb0a7100e
LeeYumi <lum0380@naver.com>
2022-06-24 14:54:54 +0900

    [AIPLATFORM-392] feat: getBizMetaDetail 상세 검색 기능 개발

--- API-SERVICE/ApiList/meta/getBizMetaDetail.py
+++ API-SERVICE/ApiList/meta/getBizMetaDetail.py
@@ -8,33 +8,30 @@ from starlette.requests import Request
 
 def api(request: Request, datasetId: str = None) -> Dict:
     user_info = get_token_info(request.headers)
-
-    if datasetId is None:
-        v_meta_wrap_query = f'SELECT * FROM v_biz_meta_wrap LIMIT 1;'
-    else:
-        v_meta_wrap_query = f'SELECT * FROM v_biz_meta_wrap WHERE biz_dataset_id = {convert_data(datasetId)}'
+    body = dict()
+    v_meta_map_query = 'SELECT kor_nm,eng_nm,nm_id FROM v_biz_meta'
+    v_meta_wrap_query = f'SELECT * FROM v_biz_meta_wrap WHERE biz_dataset_id = {convert_data(datasetId)}'
 
     try:
         db = connect_db(config.db_info)
         meta_wrap = db.select(v_meta_wrap_query)
-        # v_meta_name = db.select(v_meta_name_query)
+        meta_map = db.select(v_meta_map_query)
+
     except Exception as err:
         result = {"result": 0, "errorMessage": err}
         logger.error(err)
+
     else:
-        if datasetId is None:
-            result = make_res_msg(1, "", {}, meta_wrap[1])
-        else:
-            kor_nm_list = []
-            name_list, _ = db.select(f'SELECT eng_nm, kor_nm FROM v_biz_meta;')
-            name_info = {}
-            for name in name_list:
-                name_info[name['eng_nm']] = name['kor_nm']
-            for eng_nm in meta_wrap[1]:
-                if eng_nm in name_info:
-                    kor_nm_list.append(name_info[eng_nm])
-                else:
-                    kor_nm_list.append("")
-            result = make_res_msg(
-                1, "", meta_wrap[0][0], meta_wrap[1], kor_nm_list)
+        name_map = {name_map["eng_nm"]: name_map["kor_nm"] for name_map in meta_map[0]}
+
+        for meta_data in meta_wrap[0]:
+            body["kor_name"] = list(name_map.values())
+            body["data"] = list(meta_data.values())
+            body["eng_name"] = list(name_map.keys())
+            body["type"] = [0 for i in range(0,len(meta_map[0]))]
+            body["columnkey"] = list(meta_data.keys())
+            body["biz_dataset_id"] = datasetId
+
+        result = make_res_msg(1,"",body,meta_map[0])
+
     return result
