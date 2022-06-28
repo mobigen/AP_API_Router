29f08efbc8fe2df8025cabf4de3097457e82f32f
LeeYumi <lum0380@naver.com>
2022-06-28 17:04:26 +0900

    [AIPLATFORM-387] fix: 변수명 수정, totalcount 수정, parameter 수정

--- API-SERVICE/ApiList/meta/getBizMetaList.py
+++ API-SERVICE/ApiList/meta/getBizMetaList.py
@@ -10,7 +10,9 @@ from starlette.requests import Request
 def api(request: Request,
         perPage: int,
         curPage: int,
-        keyWordList: List[str] = Query(None)) -> Dict:
+        keywordList1: str = "",
+        keywordList2: str = "",
+        keywordList3: str = ""):
 
     user_info = get_token_info(request.headers)
     curPage = curPage - 1
@@ -24,32 +26,37 @@ def api(request: Request,
             {0}
             ) as rowNo
         from
-            meta.v_biz_meta_wrap
+            v_biz_meta_wrap
     """
 
     try:
         db = connect_db(config.db_info)
-        total_cnt = db.select(total_cnt_query)
-
-        if len(keyWordList):
+        search_word_list = [keywordList1,keywordList2,keywordList3]
+        if any(search_word_list):
             order_condition = str()
             search_condition = "data_nm like '%{0}%'"
             v_meta_wrap_query = v_meta_wrap_query + " WHERE "
+            total_cnt_query = total_cnt_query + " WHERE "
 
             # 검색 조건 추가
-            for word in keyWordList:
+            for word in search_word_list:
                 order_condition = order_condition + f"data_nm similar to '%{word}%' and "
                 v_meta_wrap_query = v_meta_wrap_query + search_condition.format(word) + " and "
+                total_cnt_query = total_cnt_query + search_condition.format(word) + " and "
 
-            # 마지막 ' and ' 삭제
+        # 마지막 ' and ' 삭제
             v_meta_wrap_query = v_meta_wrap_query[:-5]
+            total_cnt_query = total_cnt_query[:-5]
             v_meta_wrap_query = v_meta_wrap_query.format(order_condition[:-5] + " desc")
         else:
             v_meta_wrap_query = v_meta_wrap_query.format("biz_dataset_id")
-        v_meta_wrap_query = v_meta_wrap_query + f" limit {perPage}  offset ({perPage} * {curPage})"
 
+        v_meta_wrap_query = v_meta_wrap_query + f" limit {perPage}  offset ({perPage} * {curPage})"
         meta_wrap = db.select(v_meta_wrap_query)
-        meta_map = db.select(v_biz_meta_query)
+
+        total_cnt = db.select(total_cnt_query)
+        # meta_map = db.select(v_biz_meta_query)
+
     except Exception as err:
         result = {"result": 0, "errorMessage": err}
         logger.error(err)
