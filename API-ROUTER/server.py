fbc8896325d2a1b12190d237ee924ad2609684f0
CBC <39294675+cbccbs@users.noreply.github.com>
2022-06-29 16:26:56 +0900

    fix : log 경로 설정 관련 수정

--- API-ROUTER/server.py
+++ API-ROUTER/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from ApiRoute.ApiRouteConfig import config
-from Utils.CommonUtil import prepare_config
+from Utils.CommonUtil import prepare_config, set_log_path
 from ApiRoute import ApiRoute
 import os
 
@@ -11,11 +11,13 @@ app = FastAPI()
 app.include_router(api_router.router)
 
 if __name__ == '__main__':
-    log_dir = f'{config.root_path}/API-ROUTER/log'
+    log_dir = f'{config.root_path}/log'
     if os.path.isdir(log_dir):
         print(f"Directory Exists")
     else:
         print(f"Make log dir : {log_dir}")
         os.makedirs(log_dir)
+
+    set_log_path()
     uvicorn.run("server:app", host=config.server_host, port=config.server_port,
-                reload=True, log_config=f'{config.root_path}/API-ROUTER/conf/logging.conf')
+                reload=True, log_config=f'{config.root_path}/conf/logging.conf')
