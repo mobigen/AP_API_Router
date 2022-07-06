f01cbcea405411746f04994083595561a5bfc2ce
CBC <39294675+cbccbs@users.noreply.github.com>
2022-07-06 10:41:35 +0900

    feat : 쿼리 형식 동일하게 수정

--- API-SERVICE/ApiList/sitemng/getCodeGroupId.py
+++ API-SERVICE/ApiList/sitemng/getCodeGroupId.py
@@ -1,18 +1,14 @@
 from typing import Dict
 from ApiService.ApiServiceConfig import config
-from Utils.CommonUtil import connect_db, get_token_info, make_res_msg
+from Utils.CommonUtil import connect_db
 from fastapi.logger import logger
-from starlette.requests import Request
 from Utils.DataBaseUtil import convert_data
 
 
-def api(groupId, request: Request) -> Dict:
-    user_info = get_token_info(request.headers)
-
-    get_code_info_query = f'select code_id, code_nm \
-                            from tb_code_detail \
-                            where code_group_id = {convert_data(groupId)};'
-
+def api(groupId) -> Dict:
+    get_code_info_query = f'SELECT code_id, code_nm \
+                                FROM tb_code_detail \
+                            WHERE code_group_id = {convert_data(groupId)};'
     try:
         db = connect_db(config.db_info)
         code_info = db.select(get_code_info_query)
