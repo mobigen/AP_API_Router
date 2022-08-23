23ab22a9301c73ca6175265c142c3a6206d5ccf0
LeeYumi <lum0380@naver.com>
2022-08-23 17:17:05 +0900

    feat: elasticsearch data CRUD 기능 추가, annotation 수정

--- API-SERVICE/ConnectManager/ElasticSearchManager.py
+++ API-SERVICE/ConnectManager/ElasticSearchManager.py
@@ -1,4 +1,5 @@
-from elasticsearch import Elasticsearch
+from typing import Optional, Union, Dict, Any
+from elasticsearch import Elasticsearch, helpers
 from Utils.ESUtils import set_dict_list, make_query, set_find_option
 
 
@@ -7,7 +8,7 @@ class ESSearch:
             self,
             host: str = "localhost",
             port: str = "9200",
-            cur_from: int = 0,
+            cur_from: int = 1,
             size: int = 10,
             index: str = "biz_meta"
     ):
@@ -32,7 +33,7 @@ class ESSearch:
         es = Elasticsearch(f"http://{self.host}:{self.port}")
         return es
 
-    def set_body(self) -> dict:
+    def set_body(self) -> Dict[Any,Any]:
         self.body = {
             "from": 0,
             "size": 10,
@@ -42,31 +43,30 @@ class ESSearch:
         return self.body
 
     def set_sort(self, sort: str) -> None:
-        sort = sort.replace("score", "_score").split(",")
-        sort = [{field: order for field, order in [sort_item.split(":") for sort_item in sort]}]
-        self.body["sort"] = sort
+        """
+        :param sort:  type str, ex) "key1:val1,key2:val2"
+        """
+        self.body["sort"] = set_find_option(sort)
 
     def set_pagination(self) -> None:
         self.body["from"] = self.cur_from
         self.body["size"] = self.size
 
     def set_filter(self, filter_option: str) -> None:
+        """
+        :param filter_option:  type str, ex) "key1:val1,key2:val2"
+        """
         filter_list = set_find_option(filter_option)
         if len(filter_list):
             self.body["query"]["bool"]["filter"] = set_dict_list(filter_list[0], "match")
 
-    def set_match(self, operator: str = None) -> None:
+    def set_match(self, operator: Optional[str] = None) -> None:
+        """
+        :param operator: search operator, type str ex) (AND, OR)
+        """
         must_query_list = []
         field = "data_nm"
         option = "match"
-
-        # if len(self.keyword_dict.values()):
-        if len(self.keyword_dict[option]) and operator in ["or", "OR"]:
-            keyword = " ".join(self.keyword_dict[option])
-            op_query_dict = make_query(field, "query", keyword)
-            op_query_dict[field]["operator"] = operator
-            term = make_query(option, field, op_query_dict[field])
-            must_query_list.append(term)
 
         if len(self.keyword_dict[option]) and operator is None:
             must_query_list = set_dict_list(self.keyword_dict[option], option, field)
@@ -75,4 +75,35 @@ class ESSearch:
             option = "match_phrase"
             must_query_list = set_dict_list(self.keyword_dict[option],option,field)
 
+        # if len(self.keyword_dict.values()):
+        if len(self.keyword_dict[option]) and operator.upper() == "OR":
+            keyword = " ".join(self.keyword_dict[option])
+            op_query_dict = make_query(field, "query", keyword)
+            op_query_dict[field]["operator"] = operator
+            term = make_query(option, field, op_query_dict[field])
+            must_query_list.append(term)
+
         self.body["query"]["bool"]["must"] = must_query_list
+
+    def insert(self, body: dict, es_id: str) -> None:
+        self.conn.index(index=self.index, body=body, id=es_id)
+
+    def insert_bulk(self, data: list) -> None:
+        helpers.bulk(self.conn, data, index=self.index)
+
+    def update(self, body: dict, es_id: str):
+        self.conn.update(index=self.index, id=es_id, body=body)
+
+    def delete(self, field: str, data: Union[str, list]) -> None:
+        """
+        단수 : { query: { term: _id}}
+        복수 : { query : { term : []}}
+        :param field: data type str, elasticsearch index _source name
+        :param data: data type str or list
+        """
+        delete_data = {field: data}
+        delete_command = make_query("query", "term", delete_data)
+        self.conn.delete_by_query(index=self.index,body=delete_command)
+
+    def search(self) -> Dict[Any, Any]:
+        return self.conn.search(index=self.index, body=self.body)
--- API-SERVICE/Utils/ESUtils.py
+++ API-SERVICE/Utils/ESUtils.py
@@ -1,3 +1,6 @@
+from typing import Union, Optional, List, Dict, Any
+
+
 def is_space(text: str) -> int:
     if " " in text:
         return 1
@@ -5,12 +8,12 @@ def is_space(text: str) -> int:
         return 0
 
 
-def make_query(op, field, value):
+def make_query(op, field, value) -> Dict[Any,Any]:
     query = {op: {field: value}}
     return query
 
 
-def div_keyword(keyword_list: list) -> dict:
+def div_keyword(keyword_list: list) -> Dict[Any,Any]:
     keyword_dict = {"match_phrase": [], "match": []}
     for keyword in keyword_list:
         k = keyword.replace(" ","")
@@ -23,7 +26,7 @@ def div_keyword(keyword_list: list) -> dict:
     return keyword_dict
 
 
-def set_find_option(find_option: str) -> list:
+def set_find_option(find_option: str) -> List[Dict[Any, Any]]:
     """
     :param find_option: type str, ex) "key1:val1,key2:val2"
     :return: type list , ex) [{key1: val1, key2: val2}]
@@ -33,13 +36,17 @@ def set_find_option(find_option: str) -> list:
     return find_option
 
 
-def set_dict_list(item_list, operator, field=None):
+def set_dict_list(option_items: Union[list, dict],
+                  operator: str, field: Optional[str] = None
+                  ) -> List[Dict[Any, Any]]:
     query_list = []
-    for item in item_list:
+    for item in option_items:
         if field:
+            # option_item type list
             query = make_query(operator,field,item)
         else:
-            query = make_query(operator,item,item_list[item])
+            # option item type dict
+            query = make_query(operator,item,option_items[item])
         query_list.append(query)
     return query_list
 
