551c154280260705f8a4d6d33e195fa95dfc8cfe
LeeYumi <lum0380@naver.com>
2022-08-25 09:11:38 +0900

    feat: parameter, return 값 수정

--- API-SERVICE/ConnectManager/ElasticSearchManager.py
+++ API-SERVICE/ConnectManager/ElasticSearchManager.py
@@ -25,7 +25,6 @@ class ESSearch:
         self.size = size
         self.index = index
         self.cur_from = cur_from - 1
-        self.keyword_dict = dict()
         self.conn = self.connect()
         self.body = self.set_body()
 
@@ -38,13 +37,13 @@ class ESSearch:
             "from": 0,
             "size": 10,
             "sort": [],
-            "query": {"bool": [{"match_all": {}}]}
+            "query": {"bool": dict()}
         }
         return self.body
 
     def set_sort(self, sort: str) -> None:
         """
-        :param sort:  type str, ex) "key1:val1,key2:val2"
+        :param sort:  type str, ex) "field_name:asc,field_name:desc"
         """
         self.body["sort"] = set_find_option(sort)
 
@@ -60,41 +59,43 @@ class ESSearch:
         if len(filter_list):
             self.body["query"]["bool"]["filter"] = set_dict_list(filter_list[0], "match")
 
-    def set_match(self, operator: Optional[str] = None) -> None:
+    def set_match(self, keyword_dict: dict, operator: Optional[str] = None, field: str = "data_nm") -> None:
         """
+        :param keyword_dict: type dict
+        :param field: search field, type str
         :param operator: search operator, type str ex) (AND, OR)
         """
-        must_query_list = []
-        field = "data_nm"
-        option = "match"
+        if len(keyword_dict.values()):
+            must_query_list = []
+            option = "match"
 
-        if len(self.keyword_dict[option]) and operator is None:
-            must_query_list = set_dict_list(self.keyword_dict[option], option, field)
+            if len(keyword_dict[option]) and operator is None:
+                must_query_list = set_dict_list(keyword_dict[option], option, field)
 
-        if len(self.keyword_dict["match_phrase"]):
-            option = "match_phrase"
-            must_query_list = set_dict_list(self.keyword_dict[option],option,field)
+            if len(keyword_dict["match_phrase"]):
+                option = "match_phrase"
+                must_query_list = set_dict_list(keyword_dict[option],option,field)
 
-        # if len(self.keyword_dict.values()):
-        if len(self.keyword_dict[option]) and operator.upper() == "OR":
-            keyword = " ".join(self.keyword_dict[option])
-            op_query_dict = make_query(field, "query", keyword)
-            op_query_dict[field]["operator"] = operator
-            term = make_query(option, field, op_query_dict[field])
-            must_query_list.append(term)
-
-        self.body["query"]["bool"]["must"] = must_query_list
+            if len(keyword_dict[option]) and operator.upper() == "OR":
+                keyword = " ".join(keyword_dict[option])
+                op_query_dict = make_query(field, "query", keyword)
+                op_query_dict[field]["operator"] = operator
+                term = make_query(option, field, op_query_dict[field])
+                must_query_list.append(term)
+            self.body["query"]["bool"]["must"] = must_query_list
+        else:
+            self.body["query"]["bool"]["must"] = {"match_all": {}}
 
     def insert(self, body: dict, es_id: str) -> None:
-        self.conn.index(index=self.index, body=body, id=es_id)
+        return self.conn.index(index=self.index, body=body, id=es_id)
 
-    def insert_bulk(self, data: list) -> None:
-        helpers.bulk(self.conn, data, index=self.index)
+    def insert_bulk(self, data: list):
+        return helpers.bulk(self.conn, data, index=self.index)
 
     def update(self, body: dict, es_id: str):
-        self.conn.update(index=self.index, id=es_id, body=body)
+        return self.conn.update(index=self.index, id=es_id, body=body)
 
-    def delete(self, field: str, data: Union[str, list]) -> None:
+    def delete(self, field: str, data: Union[str, list]):
         """
         단수 : { query: { term: _id}}
         복수 : { query : { term : []}}
@@ -103,7 +104,7 @@ class ESSearch:
         """
         delete_data = {field: data}
         delete_command = make_query("query", "term", delete_data)
-        self.conn.delete_by_query(index=self.index,body=delete_command)
+        return self.conn.delete_by_query(index=self.index,body=delete_command)
 
     def search(self) -> Dict[Any, Any]:
         return self.conn.search(index=self.index, body=self.body)
