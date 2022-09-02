36cbec021131f27be26006aedc69752907b01925
LeeYumi <lum0380@naver.com>
2022-09-02 13:46:55 +0900

    [AIPLATFORM-477] feat: 한자연 column명 변경사항 적용

--- API-SERVICE/ApiList/meta/insertElsBizMeta.py
+++ API-SERVICE/ApiList/meta/insertElsBizMeta.py
@@ -6,28 +6,37 @@ from ConnectManager.ElasticSearchManager import ESSearch
 
 
 class BizMeta(BaseModel):
-    adm_dep: str
-    adm_dep_hp: str
-    admr_nm: str
-    copyright: str
-    ctgry: str
-    data_desc: str
-    data_nm: str
-    data_prv_desk: str
-    data_shap: str
-    data_updt_cyc: str
-    file_read_authority: str
-    file_type: str
-    kywrd: str
-    lang: str
-    license: str
-    reg_date: str
-    retv_num: str
-    rqt_dep: str
-    src_sys: str
+    biz_dataset_id: str
     src_url: str
-    updt_date: str
-    updt_nxt_date: str
+    kywrd: str
+    ctgry: str
+    data_updt_cyc: str
+    adm_dep: str
+    admr_nm: str
+    file_read_authority: str
+    retv_num: str
+    data_desc: str
+    data_prv_desk: str
+    license: str
+    lang: str
+    adm_dep_hp: str
+    data_nm: str
+    updt_nxt_dt: str
+    updt_dt: str
+    reg_dt: str
+    reg_user: str
+    amd_user: str
+    reg_date: str
+    amd_date: str
+    data_shap: str
+    data_srttn: str
+    data_limit: str
+    othr_use_notes: str
+    data_eng_nm: str
+    downl_num: str
+    attnt_data_num: str
+    share_num: str
+    contents: str
 
 
 def api(biz_meta_data: BizMeta) -> Dict:
