7b37580f6a33021e1a8641403140d4db75bc643d
CBC <39294675+cbccbs@users.noreply.github.com>
2022-09-07 14:26:10 +0900

    [AIPLATFORM-678] JWT 유효성 체크 및 재발급 기능 추가

--- API-SERVICE/ApiList/common/commonToken.py
+++ API-SERVICE/ApiList/common/commonToken.py
@@ -1,45 +1,25 @@
 from typing import Dict, Optional
-from pydantic import BaseModel
 from fastapi.logger import logger
 from datetime import datetime, timedelta
 from jose import jwt
 from Utils.CommonUtil import connect_db, get_exception_info
 from Utils.DataBaseUtil import convert_data
 from ApiService.ApiServiceConfig import config
+from starlette.requests import Request
 
 
-class IncorrectUserName(Exception):
+class InvalidUserInfo(Exception):
     pass
 
 
-class IncorrectPassword(Exception):
+class TokenDoesNotExist(Exception):
     pass
-
-
-class commonLogin(BaseModel):
-    data: Dict
-
-
-def verify_password(plain_password, hashed_password):
-    return config.pwd_context.verify(plain_password, hashed_password)
 
 
 def get_user(user_name: str):
     db = connect_db()
     user = db.select(
         f'SELECT * FROM {config.user_info["table"]} WHERE {config.user_info["id_column"]} = {convert_data(user_name)}')
-    print(f"USER: {user[0]}")
-    return user
-
-
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
-    if not user[0]:
-        raise IncorrectUserName
-
-    user = user[0][0]
-    if not verify_password(password, user[config.user_info["password_column"]]):
-        raise IncorrectPassword
     return user
 
 
@@ -56,17 +36,26 @@ def create_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 
-def api(login: commonLogin) -> Dict:
+def api(request: Request) -> Dict:
     try:
-        user = authenticate_user(
-            login.data[config.user_info["id_column"]], login.data[config.user_info["password_column"]])
+        access_token = request.headers.get(config.secret_info["header_name"])
+        if not access_token:
+            raise TokenDoesNotExist
+        payload = jwt.decode(token=access_token,
+                             key=config.secret_info["secret_key"], algorithms=config.secret_info["algorithm"])
+
+        username = payload["sub"]
+        user = get_user(username)
+        if not user[0]:
+            raise InvalidUserInfo
+        user = user[0][0]
     except Exception:
         except_name = get_exception_info()
         result = {"result": 0, "errorMessage": except_name}
     else:
-        access_token = create_token(
-            data={"sub": user[config.user_info["id_column"]]}, expires_delta=timedelta(minutes=int(config.secret_info["expire_min"])))
+        access_token = create_token(data={"sub": user[config.user_info["id_column"]]}, expires_delta=timedelta(
+            minutes=int(config.secret_info["expire_min"])))
+
         result = {"result": 1, "errorMessage": "", "data": {
             "access_token": access_token, "token_type": "bearer"}}
-
     return result
