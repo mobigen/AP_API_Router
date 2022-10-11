66539b07877818482a3137d841c7acf2033467b6
LeeYumi <lum0380@naver.com>
2022-10-11 11:16:19 +0900

    [AIPLATFORM-704] feat: els 검색 패키지를 사용하게 변경

--- API-SERVICE/ApiList/meta/getElsBizMetaList.py
+++ API-SERVICE/ApiList/meta/getElsBizMetaList.py
@@ -1,74 +1,74 @@
-from typing import Dict, Optional
-from copy import deepcopy
-from pydantic import BaseModel
-from Utils.ESUtils import div_keyword, make_query
+from typing import Dict
+from ELKSearch.Manager.manager import ElasticSearchManager
+from ELKSearch.Utils.model import InputModel
+from ELKSearch.Utils.elasticsearch_utils import make_query, base_search_query
 from Utils.CommonUtil import get_exception_info
-from ConnectManager.ElasticSearchManager import ESSearch
 
 
-class SearchOption(BaseModel):
-    perPage: int = 12
-    curPage: int = 1
-    keywordList: Optional[list] = None
-    sortOption: Optional[list] = []
-    filterOption: dict = dict()
-    dataSrttn: str = "전체"
-    filterOperator: str = "OR"
-    matchOption: str = "AND"
-
-
-def api(search_option: SearchOption) -> Dict:
-
+def api(input: InputModel) -> Dict:
     data_srttn = {
         # search_keyword: (result_key, result_data)
-        "전체": "totalCount",
         "보유데이터": "hasCount",
         "연동데이터": "innerCount",
         "외부데이터": "externalCount",
-        "해외데이터": "overseaCount"
+        "해외데이터": "overseaCount",
+        "전체": "totalCount"
     }
-    data = dict()
-    search_option.curPage = search_option.curPage - 1
-    from_page = search_option.curPage * search_option.perPage
+    data_dict = dict()
+    from_ = input.from_ - 1
+
     try:
-        # 숫자랑 특수문자 조회
-        keyword_dict = div_keyword(search_option.keywordList)
-        es = ESSearch(cur_from=from_page,size=search_option.perPage)
-        es.set_sort(search_option.sortOption)
+        es = ElasticSearchManager(page=from_, size=input.size)
+        es.set_sort(input.sortOption)
 
-        if any(search_option.filterOption.values()):
-            es.set_filter(search_option.filterOption,search_option.filterOperator)
+        ############ search option ############
+        action = "query"
+        sub_action = "must"
+        query_dict = base_search_query(action,sub_action,input.searchOption)
 
-        es.set_match(keyword_dict,search_option.matchOption)
+        # ############ filter option ############
+        sub_action = "filter"
+        item_dict = base_search_query(action,sub_action,input.filterOption)
+        query_dict.update(item_dict)
+        search_query = make_query(action,"bool", query_dict)
+        es.body.update(search_query)
+
+        # ############ sort option ############
+        sort_list = [{item.field: item.order} for item in input.sortOption]
+        es.set_sort(sort_list)
+        search_data = es.search(input.resultField)
+
+        # ############ data_srttn ############
+        i = None
+        for j,item in enumerate(item_dict["filter"]):
+            if "data_srttn" in item["match"].keys():
+                i = j
+                break
+            else:
+                i = None
 
         for ko_nm, eng_nm in data_srttn.items():
-            cnt_body_query = {"query": deepcopy(es.body["query"])}
+            if i is None:
+                cnt_query = make_query("match","data_srttn",{'operator': 'OR', 'query': ko_nm})
+                item_dict["filter"].append(cnt_query)
+                i = -1
+            else:
+                item_dict["filter"][i]["match"]["data_srttn"]["query"] = ko_nm
 
-            if "filter" not in cnt_body_query["query"]["bool"].keys():
-                cnt_body_query["query"]["bool"]["filter"] = []
+            if ko_nm == "전체":
+                del item_dict["filter"][i]
 
-            if ko_nm != "전체":
-                filter_srttn = make_query("match","data_srttn",ko_nm)
-                cnt_body_query["query"]["bool"]["filter"].append(filter_srttn)
+            query_dict.update(item_dict)
+            cnt_query = make_query("query","bool",query_dict)
+            cnt = es.conn.count(index=es.index,body=cnt_query)["count"]
+            data_dict[eng_nm] = cnt
 
-            cnt = es.conn.count(index=es.index,body=cnt_body_query)["count"]
-            data[eng_nm] = cnt
-
-        if search_option.dataSrttn != "전체":
-            filter_srttn = make_query("match","data_srttn",search_option.dataSrttn)
-
-            if "filter" not in es.body["query"]["bool"].keys():
-                es.body["query"]["bool"]["filter"] = []
-
-            es.body["query"]["bool"]["filter"].append(filter_srttn)
-
-        biz_meta_elk = es.search()
     except Exception:
         except_name = get_exception_info()
         result = {"result": 0, "errorMessage": except_name}
     else:
-        search_list = [data["_source"] for data in biz_meta_elk["hits"]["hits"]]
-        data["searchList"] = search_list
-        result = {"result": 1, "errorMessage": "", "data": data}
+        search_list = [data["_source"] for data in search_data["hits"]["hits"]]
+        data_dict["searchList"] = search_list
+        result = {"result": 1, "errorMessage": "", "data": data_dict}
 
     return result
