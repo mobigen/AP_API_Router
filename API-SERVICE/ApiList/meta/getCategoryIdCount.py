be794b178542250e7e26d354aa1075ef36b728fb
LeeYumi <lum0380@naver.com>
2022-10-11 16:53:17 +0900

    [AIPLATFORM-815] feat: 카테고리 counting 기능 추가

---
+++ API-SERVICE/ApiList/meta/getCategoryIdCount.py
@@ -0,0 +1,25 @@
+from typing import Dict
+from ELKSearch.Manager.manager import ElasticSearchManager
+from ELKSearch.Utils.elasticsearch_utils import make_query
+from Utils.CommonUtil import get_exception_info
+
+
+def api(nms) -> Dict:
+    data_dict = {}
+    key = "ctgry"
+    try:
+        ctgry_nm_list = nms.split(",")
+        es = ElasticSearchManager()
+        for c_id in ctgry_nm_list:
+            cnt_query = make_query("query","match_phrase",{key: c_id})
+            cnt = es.conn.count(index=es.index, body=cnt_query)["count"]
+            data_dict[c_id] = cnt
+
+    except Exception:
+        except_name = get_exception_info()
+        result = {"result": 0, "errorMessage": except_name}
+    else:
+        pass
+        result = {"result": 1, "errorMessage": "", "data": data_dict}
+
+    return result
