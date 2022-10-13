7ee3955fc3ba7eb04aee9185584fa5a3683f3a97
CBC <39294675+cbccbs@users.noreply.github.com>
2022-10-13 10:59:16 +0900

    [AIPLATFORM-854] login 테스트 코드 예외처리 추가

--- API-SERVICE/ServiceApiList/common/userLogin.py
+++ API-SERVICE/ServiceApiList/common/userLogin.py
@@ -38,27 +38,27 @@ sttus => SBSC
 
 
 def make_insert_query(login: dict):
-    print(f'Login Data : {login}, type : {type(login)}')
     login["reg_user"] = login["user_id"]
     login["reg_date"] = "NOW()"
     columns = ", ".join(login.keys())
     values = ", ".join(map(convert_data, login.values()))
-    print(f'INSERT INTO user_bas ({columns}) VALUES ({values});')
     return f'INSERT INTO user_bas ({columns}) VALUES ({values});'
 
 
 def api(login: userLogin) -> Dict:
     try:
         db = connect_db()
-        time_zone = 'Asia/Seoul'
-        db.execute(f"SET TIMEZONE={convert_data(time_zone)}")
-        login_query = make_insert_query(login.__dict__)
-        db.execute(login_query)
+        user_info, _ = db.select(
+            f'SELECT * FROM user_bas WHERE user_id = {convert_data(login.user_id)};')
+        if not user_info:
+            time_zone = 'Asia/Seoul'
+            db.execute(f"SET TIMEZONE={convert_data(time_zone)}")
+            login_query = make_insert_query(login.__dict__)
+            db.execute(login_query)
     except Exception:
         except_name = get_exception_info()
         result = {"result": 0, "errorMessage": except_name}
     else:
-
         result = {"result": 1, "errorMessage": ""}
 
     return result
