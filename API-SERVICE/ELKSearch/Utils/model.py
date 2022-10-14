5d8d94672eb8e26af431ca88ab9873ab3acee1eb
LeeYumi <lum0380@naver.com>
2022-10-14 13:51:57 +0900

    [AIPLATFORM-878] chore: default index 수정

--- API-SERVICE/ELKSearch/Utils/model.py
+++ API-SERVICE/ELKSearch/Utils/model.py
@@ -14,7 +14,7 @@ class SortOption(BaseModel):
 
 
 class InputModel(BaseModel):
-    index: str = "biz_meta"
+    index: str = "kt_biz_meta"
     from_: int = Field(1, alias="from")
     size: int = 10
     resultField: list = []
