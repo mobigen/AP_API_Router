9429806a0b96a6807008aeff25f59e9d67ae4bfd
LeeYumi <lum0380@naver.com>
2022-10-18 09:52:40 +0900

    [AIPLATFORM-903] feat: data_type을 기준으로 데이터 갯수 counting 기능 추가

--- API-SERVICE/ServiceApiList/meta/getElsBizMetaList.py
+++ API-SERVICE/ServiceApiList/meta/getElsBizMetaList.py
@@ -8,9 +8,15 @@ from ServiceUtils.CommonUtil import get_exception_info
 def api(input: InputModel) -> Dict:
     data_dict = dict()
     from_ = input.from_ - 1
-
+    data_type = {
+        # search_keyword: (result_key, result_data)
+        "C": "contentsCount",
+        "A": "assetsCount",
+        "M": "metaCount",
+        "T": "totalCount"
+    }
     try:
-        es = ElasticSearchManager(page=from_, size=input.size)
+        es = ElasticSearchManager(index=input.index, page=from_, size=input.size)
         es.set_sort(input.sortOption)
 
         action = "query"
@@ -30,6 +36,22 @@ def api(input: InputModel) -> Dict:
         es.set_sort(sort_list)
         search_data = es.search(input.resultField)
 
+        i = None
+        for j,item in enumerate(item_dict["filter"]):
+            if "data_type" in item["match"].keys():
+                i = j
+                break
+
+        for key_nm, eng_nm in data_type.items():
+            item_dict["filter"][i]["match"]["data_type"]["query"] = key_nm
+
+            if key_nm == "T":
+                del item_dict["filter"][i]
+
+            query_dict.update(item_dict)
+            cnt_query = make_query("query","bool",query_dict)
+            cnt = es.conn.count(index=es.index,body=cnt_query)["count"]
+            data_dict[eng_nm] = cnt
     except Exception:
         except_name = get_exception_info()
         result = {"result": 0, "errorMessage": except_name}
