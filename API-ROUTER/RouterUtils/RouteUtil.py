2b8ea15a5616ca0dcaea17f51b743af853b65085
CBC <39294675+cbccbs@users.noreply.github.com>
2022-10-31 16:45:42 +0900

    api param 설정 이슈 수정

--- API-ROUTER/RouterUtils/RouteUtil.py
+++ API-ROUTER/RouterUtils/RouteUtil.py
@@ -33,14 +33,13 @@ def make_route_response(result, api_name, access_token):
 
 def get_api_info(route_url):
     api_info = None
-    api_params = None
+    api_params = []
     for api in config.api_info:
         if api["route_url"] == route_url:
             api_info = api
             for params in config.api_params:
                 if params["api_nm"] == api["api_nm"]:
-                    api_params = params
-                    break
+                    api_params.append(params)
             break
     return api_info, api_params
 
@@ -95,15 +94,16 @@ async def call_remote_func(api_info, api_params, input_params) -> Dict:
     from RouterUtils.CommonUtil import get_exception_info
 
     command_input = ""
-    try:
-        data = input_params[api_params["nm"]]
-        if not data:
-            data = api_params["deflt_val"]
-        command_input += f' --{api_params["nm"]} {data}'
-    except KeyError:
-        logger.error(
-            f'parameter set default value. [{api_params["nm"]}]')
-        command_input += f' --{api_params["nm"]} {api_params["deflt_val"]}'
+    for api_param in api_params:
+        try:
+            data = input_params[api_param["nm"]]
+            if not data:
+                data = api_param["deflt_val"]
+            command_input += f' --{api_param["nm"]} {data}'
+        except KeyError:
+            logger.error(
+                f'parameter set default value. [{api_param["nm"]}]')
+            command_input += f' --{api_param["nm"]} {api_param["deflt_val"]}'
 
     cmd = f'{api_info["cmd"]} {command_input}'
 
