097ebb2b05bec30433bc9c646b2a6dacb6cddadf
LeeYumi <lum0380@naver.com>
2022-11-16 08:58:23 +0900

    fix: elasticsearch config 코드 적용

--- API-SERVICE/ApiList/meta/deleteElsBizMeta.py
+++ API-SERVICE/ApiList/meta/deleteElsBizMeta.py
@@ -1,10 +1,13 @@
 from Utils.CommonUtil import get_exception_info
 from ELKSearch.Manager.manager import ElasticSearchManager
+from ApiService.ApiServiceConfig import config
+from ELKSearch.Utils.database_utils import get_config
 
 
 def api(biz_dataset_id: str):
+    els_config = get_config(config.root_path,"config.ini")[config.db_type[:-3]]
     try:
-        es = ElasticSearchManager()
+        es = ElasticSearchManager(**els_config)
         es.delete("biz_dataset_id", biz_dataset_id)
 
     except Exception:
--- API-SERVICE/ApiList/meta/getCategoryNmCount.py
+++ API-SERVICE/ApiList/meta/getCategoryNmCount.py
@@ -2,14 +2,17 @@ from typing import Dict
 from ELKSearch.Manager.manager import ElasticSearchManager
 from ELKSearch.Utils.elasticsearch_utils import make_query
 from Utils.CommonUtil import get_exception_info
+from ELKSearch.Utils.database_utils import get_config
+from ApiService.ApiServiceConfig import config
 
 
 def api(nms) -> Dict:
     data_dict = {}
     key = "ctgry"
+    els_config = get_config(config.root_path,"config.ini")[config.db_type[:-3]]
     try:
         ctgry_nm_list = nms.split(",")
-        es = ElasticSearchManager()
+        es = ElasticSearchManager(**els_config)
         for c_id in ctgry_nm_list:
             cnt_query = make_query("query","match_phrase",{key: c_id})
             cnt = es.conn.count(index=es.index, body=cnt_query)["count"]
