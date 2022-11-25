bde9b20791a85c7035b700a72f4b5b4a19dcf03f
seokwoo-yang <ysw424@gmail.com>
2022-11-25 10:38:19 +0900

    fix: 원격 프로시저콜 시 파라미터의 띄어쓰기 허용토록 변경

--- API-ROUTER/Utils/RouteUtil.py
+++ API-ROUTER/Utils/RouteUtil.py
@@ -15,8 +15,7 @@ def make_url(server_name: str, url_path: str):
                 netloc = server_info["ip_adr"]
             else:
                 netloc = server_info["domn_nm"]
-            url = ParseResult(
-                scheme="http", netloc=netloc, path=url_path, params="", query="", fragment="")
+            url = ParseResult(scheme="http", netloc=netloc, path=url_path, params="", query="", fragment="")
             logger.info(f"Message Passing Url : {url.geturl()}")
             return url.geturl()
     return None
@@ -26,8 +25,7 @@ def make_route_response(result, api_name, access_token):
     response = JSONResponse(content=result)
     add_cookie_api_list = config.secret_info["add_cookie_api"].split(",")
     if api_name in add_cookie_api_list:
-        response.set_cookie(
-            key=config.secret_info["cookie_name"], value=access_token)
+        response.set_cookie(key=config.secret_info["cookie_name"], value=access_token)
     return response
 
 
@@ -60,26 +58,29 @@ async def bypass_msg(api_info, params_query, body, headers):
                     params[parser_param[0]] = parser_param[1]
 
             async with session.get(url, params=params, headers=headers) as response:
-                access_token = response.cookies.get(
-                    config.secret_info["cookie_name"])
+                access_token = response.cookies.get(config.secret_info["cookie_name"])
                 result = await response.json()
         elif method == "POST":
             async with session.post(url, json=body, headers=headers) as response:
-                access_token = response.cookies.get(
-                    config.secret_info["cookie_name"])
+                access_token = response.cookies.get(config.secret_info["cookie_name"])
                 result = await response.json()
         else:
-            logger.error(f'Method Not Allowed. {method}')
+            logger.error(f"Method Not Allowed. {method}")
             result = {"result": 0, "errorMessage": "Method Not Allowed."}
     return result, access_token
 
 
 async def run_cmd(cmd: str):
-    async with asyncssh.connect(host=config.remote_info["host"], port=int(config.remote_info["port"]),
-                                username=config.remote_info["id"], password=config.remote_info["password"], known_hosts=None) as conn:
-        logger.info(f'Run Cmd : {cmd}')
+    async with asyncssh.connect(
+        host=config.remote_info["host"],
+        port=int(config.remote_info["port"]),
+        username=config.remote_info["id"],
+        password=config.remote_info["password"],
+        known_hosts=None,
+    ) as conn:
+        logger.info(f"Run Cmd : {cmd}")
         result = await conn.run(cmd, check=True)
-        logger.info(f'Command Result : {result.stdout}')
+        logger.info(f"Command Result : {result.stdout}")
     return result.stdout
 
 
@@ -90,10 +91,9 @@ async def call_remote_func(api_info, api_params, input_params) -> Dict:
             data = input_params[api_param["nm"]]
             if not data:
                 data = api_param["deflt_val"]
-            command_input += f' --{api_param["nm"]} {data}'
+            command_input += f' --{api_param["nm"]} "{data}"'
         except KeyError:
-            logger.error(
-                f'parameter set default value. [{api_param["nm"]}]')
+            logger.error(f'parameter set default value. [{api_param["nm"]}]')
             command_input += f' --{api_param["nm"]} {api_param["deflt_val"]}'
 
     cmd = f'{api_info["cmd"]} {command_input}'
