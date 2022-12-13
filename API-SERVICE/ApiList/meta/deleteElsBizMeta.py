ea3456dcd03b495abccae558132f4084fc92c969
LeeYumi <lum0380@naver.com>
2022-12-13 15:45:45 +0900

    [AIPLATFORM-1308] fix: data body parameter 수정

--- API-SERVICE/ApiList/meta/deleteElsBizMeta.py
+++ API-SERVICE/ApiList/meta/deleteElsBizMeta.py
@@ -1,15 +1,20 @@
 from typing import Dict
+from pydantic import BaseModel
 from Utils.CommonUtil import get_exception_info
 from ELKSearch.Manager.manager import ElasticSearchManager
 from ApiService.ApiServiceConfig import config
 from ELKSearch.Utils.database_utils import get_config
 
 
-def api(biz_dataset_id: str) -> Dict:
+class DeleteData(BaseModel):
+    biz_dataset_id: str
+
+
+def api(input: DeleteData) -> Dict:
     els_config = get_config(config.root_path, "config.ini")[config.db_type[:-3]]
     try:
         es = ElasticSearchManager(**els_config)
-        es.delete("biz_dataset_id", biz_dataset_id)
+        es.delete("biz_dataset_id", input.biz_dataset_id)
 
     except Exception:
         except_name = get_exception_info()
