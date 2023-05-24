f53f8505127fce3f6ecda8386819250a76ff7ffe
LeeYumi <lum0380@naver.com>
2023-05-24 10:53:50 +0900

    [AIPLATFORM-1792] chore: 주석 추가

--- API_SERVICE/meta_service/ELKSearch/index.py
+++ API_SERVICE/meta_service/ELKSearch/index.py
@@ -8,10 +8,18 @@ class Index:
     def __init__(self, connect):
         self.connect = connect
 
-    def all_index(self):
-        return self.connect.cat.indices()
+    def all_index(self) -> dict:
+        """
+        :return: key값이 index명, value는 alias 설정 값이다
+        """
+        return self.connect.indices.get_alias(index="*")
 
     def create(self, index: str, path: str = None) -> dict:
+        """
+        :param index: 생성할 index 이름
+        :param path: 생성할 index의 mapping 파일 위치
+        :return: els에 요청한 결과 성공/실패는 bool 타입으로 반환 된다
+        """
         if path is None:
             path = f"{ELSSearch_PATH}/mapping/{index}.json"
 
@@ -21,4 +29,8 @@ class Index:
         return self.connect.indices.create(index=index, body=mapping)
 
     def delete(self, index: str) -> dict:
+        """
+        :param index: 삭제할 index의 이름
+        :return: els에 요청한 결과 성공/실패는 bool 타입으로 반환 된다
+        """
         return self.connect.indices.delete(index=index, ignore=[400,404])
