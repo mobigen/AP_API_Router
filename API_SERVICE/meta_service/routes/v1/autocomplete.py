343f195021773983369c54fe2a5d07b6d68a2dbe
LeeYumi <lum0380@naver.com>
2023-05-24 15:44:30 +0900

    [AIPLATFORM-1792] feat: 검색어 자동완성 모듈 추가

--- API_SERVICE/meta_service/routes/v1/autocomplete.py
+++ API_SERVICE/meta_service/routes/v1/autocomplete.py
@@ -4,6 +4,15 @@ from fastapi import APIRouter
 
 from meta_service.database.conn import db
 from libs.database.connector import Connector
+from meta_service.ELKSearch.config import local_server
+from meta_service.common.search import common_prefix
+
+from pydantic import BaseModel
+
+
+class Prefix(BaseModel):
+    col_nm: str
+    keyword: str
 
 
 router = APIRouter()
@@ -11,6 +20,13 @@ router = APIRouter()
 logger = logging.getLogger()
 
 
-@router.post("")
-def test():
-    pass
+@router.post("autocomplete", response_model=dict)
+def autocomplete(input: Prefix, size: int):
+    index = ""
+
+    try:
+        result = common_prefix(local_server, index, size, input)
+    except Exception as e:
+        result = {"result": 0, "errorMessage": str(e)}
+        logger.error(e, exc_info=True)
+    return result
