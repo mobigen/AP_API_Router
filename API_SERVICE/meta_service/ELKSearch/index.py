3f0bf6eccbc8d119b96fcf02aea9512485e949e6
LeeYumi <lum0380@naver.com>
2023-05-24 10:43:10 +0900

    [AIPLATFORM-1792]: feat: index 관리 기능 추가

--- API_SERVICE/meta_service/ELKSearch/index.py
+++ API_SERVICE/meta_service/ELKSearch/index.py
@@ -1,8 +1,24 @@
+import os
+import json
+# ELKSearch 경로
+ELSSearch_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 class Index:
-    def __init__(self):
-        pass
+    def __init__(self, connect):
+        self.connect = connect
 
-    def update(self):
-        pass
+    def all_index(self):
+        return self.connect.cat.indices()
+
+    def create(self, index: str, path: str = None) -> dict:
+        if path is None:
+            path = f"{ELSSearch_PATH}/mapping/{index}.json"
+
+        with open(path, "r") as fp:
+            mapping = json.load(fp)
+
+        return self.connect.indices.create(index=index, body=mapping)
+
+    def delete(self, index: str) -> dict:
+        return self.connect.indices.delete(index=index, ignore=[400,404])
