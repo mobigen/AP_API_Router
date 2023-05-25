eaa21b1d9147c6f9b56495ac761fc0c965ffe290
LeeYumi <lum0380@naver.com>
2023-05-25 10:23:48 +0900

    [AIPLATFORM-1792] feat: 검색, 자동완성 api 수정

--- API_SERVICE/meta_service/routes/v1/autocomplete.py
+++ API_SERVICE/meta_service/routes/v1/autocomplete.py
@@ -1,33 +1,39 @@
-import logging
+# import logging
 
 from fastapi import APIRouter
 
 from meta_service.database.conn import db
 from libs.database.connector import Connector
-from meta_service.ELKSearch.config import local_server
-from meta_service.common.search import common_prefix
 
-from pydantic import BaseModel
+from meta_service.ELKSearch.config import local_server
+from meta_service.ELKSearch.Utils.document_utils import search_filter
+
+from meta_service.common.search import default_search_set
+
+
+from pydantic import BaseModel, Field
 
 
 class Prefix(BaseModel):
+    index: str
     size: int
+    from_: int = Field(0, alias="from")
     col_nm: str
     keyword: str
 
 
 router = APIRouter()
 
-logger = logging.getLogger()
+# logger = logging.getLogger()
 
 
-@router.post("autocomplete", response_model=dict)
+@router.post("/autocomplete", response_model=dict)
 def autocomplete(input: Prefix):
-    index = ""
-
     try:
-        result = common_prefix(local_server, index, input)
+        query = {input.col_nm: input.keyword}
+        docmanager = default_search_set(local_server, input.index, input.size)
+        result = {"result": 0,"data": search_filter(docmanager.prefix(body=query))}
     except Exception as e:
         result = {"result": 0, "errorMessage": str(e)}
-        logger.error(e, exc_info=True)
+        # logger.error(e, exc_info=True)
     return result
--- API_SERVICE/meta_service/routes/v1/els_data_search.py
+++ API_SERVICE/meta_service/routes/v1/els_data_search.py
@@ -4,39 +4,39 @@ from fastapi import APIRouter
 
 from meta_service.database.conn import db
 from libs.database.connector import Connector
+
 from meta_service.ELKSearch.config import local_server
-from meta_service.ELKSearch.model import InputModel, CoreOption
-from meta_service.ELKSearch.document import DocumentManager
-from meta_service.ELKSearch.Utils.base import set_els, make_format
+from meta_service.ELKSearch.model import InputModel
+from meta_service.ELKSearch.Utils.base import make_format
+from meta_service.ELKSearch.Utils.document_utils import search_filter
+
+from meta_service.common.search import default_search_set
 
 
 router = APIRouter()
 
 logger = logging.getLogger()
 
-
-@router.post("search", response_model=dict)
+@router.post("/search", response_model=dict)
 def search(input: InputModel):
     """
 
     :param input:
     :return:
     """
-    index = "kt_biz_meta"
     try:
-        es = set_els(local_server)
-        docmanager = DocumentManager(es, index)
-        docmanager.set_pagination(input.size, input.from_)
+        docmanager = default_search_set(local_server, input.index, input.size, input.from_)
 
         # 임시
-        search_option = CoreOption(**input.searchOption[0])
+        search_option = input.searchOption[0]
         search_query = make_format(
             "query",
             "match",
             {search_option.field[0]: search_option.keywords[0]}
         )
 
-        docmanager.search()
+        docmanager.set_body(search_query)
+        result = {"result": 0, "data": search_filter(docmanager.find(input.resultField))}
     except Exception as e:
         result = {"result": 0, "errorMessage": str(e)}
         logger.error(e, exc_info=True)
