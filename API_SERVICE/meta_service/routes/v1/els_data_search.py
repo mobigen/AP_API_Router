4d97476fe32cf07848900e6ee5c169e8caead4f2
LeeYumi <lum0380@naver.com>
2023-06-01 08:58:50 +0900

    [AIPLATFORM-1792] fix: 개발 서버 설정으로 변경

--- API_SERVICE/meta_service/routes/v1/els_data_search.py
+++ API_SERVICE/meta_service/routes/v1/els_data_search.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter
 from meta_service.database.conn import db
 from libs.database.connector import Connector
 
-from meta_service.ELKSearch.config import local_server
+from meta_service.ELKSearch.config import dev_server
 from meta_service.ELKSearch.model import InputModel
 from meta_service.ELKSearch.Utils.base import make_format
 from meta_service.ELKSearch.Utils.document_utils import search_filter
@@ -25,7 +25,8 @@ def search(input: InputModel):
     :return:
     """
     try:
-        docmanager = default_search_set(local_server, input.index, input.size, input.from_)
+        # 숫자 검색 조건에 포함
+        docmanager = default_search_set(dev_server, input.index, input.size, input.from_)
 
         # 임시
         search_option = input.searchOption[0]
