ccac00fa853773d769d08c33102a9c30b5df5c97
seokwoo-yang <ysw424@gmail.com>
2023-06-07 16:11:09 +0900

    fix: common execute에서 login 관련 테이블에 접근 불가토록 변경

--- API_SERVICE/common_service/routes/v1/execute.py
+++ API_SERVICE/common_service/routes/v1/execute.py
@@ -4,9 +4,11 @@ from typing import Dict, List, Optional
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from starlette.responses import JSONResponse
+from common_service.common.const import NOT_ALLOWED_TABLES
 
 from common_service.database.conn import db
 from libs.database.connector import Executor
+
 
 logger = logging.getLogger()
 
@@ -25,6 +27,8 @@ router = APIRouter()
 async def common_execute(params: List[CommonExecute], session: Executor = Depends(db.get_db)):
     try:
         for param in params:
+            if param.table_nm in NOT_ALLOWED_TABLES:
+                return JSONResponse(content={"result": 0, "errorMessage": "NotAllowedTable"})
             session.execute(**param.dict())
         return JSONResponse(content={"result": 1, "errorMessage": ""}, status_code=200)
     except Exception as e:
