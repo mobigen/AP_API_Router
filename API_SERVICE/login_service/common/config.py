55353b3e42e606c2960c752d1b551a7870a54a8c
seokwoo-yang <ysw424@gmail.com>
2023-07-05 13:44:17 +0900

    fix: login service 코드정리

--- API_SERVICE/login_service/common/config.py
+++ API_SERVICE/login_service/common/config.py
@@ -7,7 +7,7 @@ from typing import Union
 from pydantic import BaseSettings, PostgresDsn, validator, SecretStr
 
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(f"project base_dir :: {base_dir}")
+print(f"login base_dir :: {base_dir}")
 
 
 class DBInfo(BaseSettings):
@@ -16,6 +16,10 @@ class DBInfo(BaseSettings):
     USER: str = ""
     PASS: SecretStr = ""
     BASE: str = ""
+
+    class Config:
+        env_file = f"{base_dir}/.env"
+        env_file_encoding = "utf-8"
 
     def get_dsn(self):
         return ""
@@ -63,12 +67,10 @@ class Settings(BaseSettings):
 
 
 class ProdSettings(Settings):
-    RELOAD = False
-    TESTING = False
-
-    class Config:
-        env_file = f"{base_dir}/.env"
-        env_file_encoding = "utf-8"
+    TESTING: bool = False
+    DB_POOL_RECYCLE: int = 900
+    DB_ECHO: bool = True
+    RELOAD: bool = False
 
 
 class LocalSettings(Settings):
@@ -85,8 +87,6 @@ class LocalSettings(Settings):
         BASE="dataportal",
         SCHEMA="sitemng,users,meta,iag,ckan,board,analysis",
     )
-
-    # DB_INFO: TiberoInfo = TiberoInfo(HOST="192.168.101.164", PORT="8629", USER="dhub", PASS="dhub1234", BASE="tibero")
 
 
 class TestSettings(LocalSettings):
