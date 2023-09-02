7ff6b3a85f1a6b6fc6b70ef36be508541b0d1247
swish1995 <swish1995@gmail.com>
2023-09-02 19:17:13 +0900

    changfe: cookie domain name 수정

--- API_SERVICE/login_service/routes/v2/auth.py
+++ API_SERVICE/login_service/routes/v2/auth.py
@@ -226,7 +226,7 @@ async def login(params: LoginInfoWrap, session: Executor = Depends(db.get_db)) -
 
     if token["status_code"] == 200:
         response = JSONResponse(status_code=200, content={"result": 1, "errorMessage": ""})
-        response.set_cookie(key=COOKIE_NAME, value=token)
+        response.set_cookie(key=COOKIE_NAME, value=token, domain="bigdata-car.kr")
         return response
     else :
         return JSONResponse(
@@ -241,7 +241,7 @@ async def loginSocial(request: Request, params: RegisterInfoWrap, session: Execu
     token = await get_social_token(**param.dict())
     if token["status_code"] == 200:
         response = JSONResponse(status_code=200, content={"result": 1, "errorMessage": ""})
-        response.set_cookie(key=COOKIE_NAME, value=token)
+        response.set_cookie(key=COOKIE_NAME, value=token, domain="bigdata-car.kr")
         return response
     else :
         return JSONResponse(
