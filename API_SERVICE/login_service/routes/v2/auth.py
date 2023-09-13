4e9021e6e2515778be1233d6cce4939548281d67
swish1995 <swish1995@gmail.com>
2023-09-13 17:10:42 +0900

    fix: domain 관련 수정

--- API_SERVICE/login_service/routes/v2/auth.py
+++ API_SERVICE/login_service/routes/v2/auth.py
@@ -151,9 +151,9 @@ router = APIRouter()
 @router.post("/user/v2/commonLogout")
 async def logout():
     response = JSONResponse(status_code=200, content={"result": 1, "errorMessage": ""})
-    response.delete_cookie(COOKIE_NAME, domain="bigdata-car.kr")
+    response.delete_cookie(COOKIE_NAME, domain=".bigdata-car.kr")
     # studio cookie 삭제
-    response.delete_cookie("x-access-token", domain="bigdata-car.kr")
+    response.delete_cookie("x-access-token", domain=".bigdata-car.kr")
     return response
 
 @router.get("/user/v2/commonUserInfo")
