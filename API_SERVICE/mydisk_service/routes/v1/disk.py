6399260cd9b9e1a28396f50ef3ded8ea334f4c84
swish1995 <swish1995@gmail.com>
2023-09-20 16:30:31 +0900

    add: mydisk/v1/copy, mydisk/v1/link

--- API_SERVICE/mydisk_service/routes/v1/disk.py
+++ API_SERVICE/mydisk_service/routes/v1/disk.py
@@ -1,9 +1,10 @@
 import os
 import logging
+import shutil
 import pandas as pd
 
 from pathlib import Path
-from typing import Dict
+from typing import Union, Dict
 
 from fastapi import APIRouter, Depends, Request
 from pydantic import BaseModel
@@ -12,8 +13,29 @@ from libs.auth.keycloak import keycloak
 from libs.disk.mydisk import mydisk
 from mydisk_service.common.config import settings
 
-
 logger = logging.getLogger()
+
+class CopyParams(BaseModel):
+    src_path: str
+    force: Union[bool, str]
+    dst_path: str
+
+    def is_force(self):
+        if isinstance(self.force, bool):
+            return self.force
+        elif self.force in ("yes", "true", "t", "y", "1"):
+            return True
+        return False
+
+    def get_src(self):
+        return os.path.join(
+            settings.MYDISK_ROOT_DIR, self.src_path.lstrip("/") if self.src_path.startswith("/") else self.src_path
+        )
+
+    def get_dst(self):
+        return os.path.join(
+            settings.MYDISK_ROOT_DIR, self.dst_path.lstrip("/") if self.dst_path.startswith("/") else self.dst_path
+        )
 
 class TreeParams(BaseModel):
     target_directory: str
@@ -83,6 +105,45 @@ async def walk(param: TreeParams) -> Dict:
     except Exception as e:
         result = {"result": 0, "errorMessage": str(e), "data": []}
     return result
+
+@router.post("/v1/link")
+async def hardlink(params: CopyParams):
+    try:
+        run(params.get_src(), params.get_dst(), params.is_force(), False)
+        result = {"result": 1, "errorMessage": "", "data": {"body": 200}}
+    except Exception as e:
+        result = {"result": 1, "errorMessage": str(e)}
+    return result
+
+
+@router.post("/v1/copy")
+async def copy(params: CopyParams):
+    try:
+        run(params.get_src(), params.get_dst(), params.is_force(), True)
+        result = {"result": 1, "errorMessage": "", "data": {"body": 200}}
+    except Exception as e:
+        result = {"result": 1, "errorMessage": str(e)}
+    return result
+
+
+def run(src_path, dst_path, is_force, is_copy):
+    os.makedirs(os.path.dirname(dst_path), exist_ok=True)
+
+    if os.path.exists(dst_path):
+        if is_force:
+            shutil.rmtree(dst_path)
+        else:
+            raise Exception("alreay exist")
+
+    if os.path.isfile(src_path):
+        os.link(src_path, dst_path)
+    else:
+        shutil.copytree(
+            src=src_path,
+            dst=dst_path,
+            dirs_exist_ok=is_force,
+            copy_function=os.link if not is_copy else shutil.copy2,
+        )
 
 async def get_admin_token() -> None:
     res = await mydisk.generate_admin_token(
