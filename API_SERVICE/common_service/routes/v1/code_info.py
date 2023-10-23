84a27cbcc40ef6fc7073f28ac69eea8ae16b4819
seokwoo-yang <ysw424@gmail.com>
2023-10-23 10:13:53 +0900

    fix: code info where 조건문 오류 수정

--- API_SERVICE/common_service/routes/v1/code_info.py
+++ API_SERVICE/common_service/routes/v1/code_info.py
@@ -10,7 +10,11 @@ router = APIRouter()
 
 @router.get("/getCodeInfo")
 async def get_code_detail(groupId, session: Executor = Depends(db.get_db)) -> Dict:
-    rows, _ = session.query(table_nm="tb_code_detail", key="code_group_id", value=groupId).all()
+    table_nm = "tb_code_detail"
+    rows, _ = session.query(
+        table_nm=table_nm,
+        where_info=[{"table_nm": table_nm, "key": "code_group_id", "value": groupId, "compare_op": "="}],
+    ).all()
     code_infos = [
         {"code_id": row["code_id"], "code_nm": row["code_nm"], "data_1": row["data_1"], "data_2": row["data_2"]}
         if rows
