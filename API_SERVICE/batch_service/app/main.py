f9963ec8519a57205e02f273c1534de06800e12b
LeeYumi <lum0380@naver.com>
2024-01-17 16:54:32 +0900

    chore: 주석 추가

--- API_SERVICE/batch_service/app/main.py
+++ API_SERVICE/batch_service/app/main.py
@@ -4,8 +4,8 @@ from fastapi import FastAPI
 
 from batch_service.app.common.config import settings
 from batch_service.app.database.conn import db #,seoul_db
-from batch_service.app.jobs import send_email, recommend_word, els_update, seoul_db_upload
-from batch_service.app.routes.v1 import els_update#, seoul_update
+from batch_service.app.jobs import send_email, recommend_word, els_update#, seoul_db_upload
+# from batch_service.app.routes.v1 import els_update, seoul_update
 
 """
 서울대 DB connection Error 발생 이슈로 관련 코드 전부 주석처리
@@ -18,7 +18,7 @@ def create_app():
 
     # seoul_db.init_app(app_, DB_INFO=settings.SEOUL_DB_INFO.dict(by_alias=True), **settings.dict(exclude={"DB_INFO"}))
 
-    app_.include_router(els_update.router, prefix="/portal/api/batch")
+    # app_.include_router(els_update.router, prefix="/portal/api/batch")
 
     return app_
 
