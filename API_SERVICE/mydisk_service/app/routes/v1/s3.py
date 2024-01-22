f915fbaadd16ec72f4675a725b7f6048968b62fa
양석우 <ysw424@gmail.com>
2024-01-22 13:24:02 +0900

    change: upload 시 prefix 제공(#1321)

--- API_SERVICE/mydisk_service/app/routes/v1/s3.py
+++ API_SERVICE/mydisk_service/app/routes/v1/s3.py
@@ -131,7 +131,7 @@ async def get_object(
 
 
 @router.post("/object")
-async def upload_object(bucket_name: str, object_path: Optional[str] = "", s3=Depends(get_s3_client)):
+async def upload_object(bucket_name: str, prefix: Optional[str] = "", object_path: Optional[str] = "", s3=Depends(get_s3_client)):
     try:
         s3.head_bucket(Bucket=bucket_name)
     except ClientError:
@@ -147,7 +147,7 @@ async def upload_object(bucket_name: str, object_path: Optional[str] = "", s3=De
                 for file in files:
                     local_path = os.path.join(root, file)
                     s3_path = os.path.relpath(local_path, local_base_dir)
-                    await upload_file_one(bucket_name=bucket_name, local_path=local_path, s3_path=s3_path, s3=s3)
+                    await upload_file_one(bucket_name=prefix+bucket_name, local_path=local_path, s3_path=s3_path, s3=s3)
                     logger.debug(f"Object {local_path} uploaded to {bucket_name}/{s3_path} successfully")
         else:
             await upload_file_one(bucket_name, object_full_path, object_path, s3)
