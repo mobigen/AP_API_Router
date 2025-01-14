407784f999a3bc98c9c6d0f792d19c5b92a4d895
swish1995 <swish1995@gmail.com>
2025-01-14 15:07:05 +0900

    feat: 서울대 DB 연동 관련 els 초기화 코드 추가

--- API_SERVICE/batch_service/app/jobs/seoul_db_upload.py
+++ API_SERVICE/batch_service/app/jobs/seoul_db_upload.py
@@ -1,8 +1,12 @@
 import logging
 from datetime import datetime
 
-from batch_service.app.common.const import SeoulDataKor, SeoulDataKorKatech, SeoulDataWorld, SeoulDataWorldKatech
+from batch_service.app.common.const import SeoulDataKor, SeoulDataKorKatech, SeoulDataWorld, SeoulDataWorldKatech, BizDataTable, CkanDataTable
 from batch_service.app.database.conn import seoul_db, db
+from batch_service.app.common.config import settings, base_dir
+from batch_service.app.common.utils import default_search_set, data_process, index_set
+from libs.els.ELKSearch.Utils.base import set_els
+from libs.els.ELKSearch.index import Index
 
 logger = logging.getLogger()
 
@@ -28,6 +32,7 @@ def insert_db(kor_check: bool = True):
         update_index, insert_index = 0, 0
         with db.get_db_manager() as sess:
             for data_dict in dataset:
+                data_dict["modified_dt"] = data_dict["cat_ver"]
                 check_query = table_katech.get_select_query(data_dict[table.key_column])
                 if sess.query(**check_query).first():
                     # update
@@ -47,3 +52,31 @@ def insert_db(kor_check: bool = True):
     except Exception as e:
         logger.info(data_dict)
         print(e)
+
+
+def init_els():
+    es = set_els(host=settings.ELS_INFO.ELS_HOST, port=settings.ELS_INFO.ELS_PORT)
+    logger.info(Index(es).init_els_all_index(f"{base_dir}/resources/mapping"))
+
+    bulk_index("biz_meta", BizDataTable)
+    bulk_index("v_biz_meta_oversea_els", CkanDataTable)
+
+def bulk_index(index_nm, table):
+    try:
+        docmanager = default_search_set(host=settings.ELS_INFO.ELS_HOST, port=settings.ELS_INFO.ELS_PORT, index=index_nm)
+
+        query = table.get_select_query("")
+        query.pop("where_info")
+        with db.get_db_manager() as sess:
+            data_list = sess.query(**query).all()[0]
+            logger.info(len(data_list))
+
+            for data_dict in data_list:
+                insert_body = data_process(data_dict)
+                docmanager.set_body(insert_body["_source"])
+                res = docmanager.insert(insert_body["_id"])
+                logger.info(res)
+    except Exception as e:
+        logger.info(e)
+        print(e)
+
